7e50cec4bba1bc76869db483e45ec25ce603838f
Sam Reichle <samjhreichle@gmail.com>
2020-07-16 09:42:38 -0500

    Setup prodcution webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,53 @@
 // input-> entry point -> output 
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// __dirname is absolute path
-module.exports = {
-    entry: './src/app.js',
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            // tests to run for any file that ends in .js
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+    return  {
+        entry: './src/app.js',
+        output: {
+            // __dirname is absolute path
+          path: path.join(__dirname, 'public'),
+          filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                // tests to run for any file that ends in .js
+                test: /\.js$/,
+                exclude: /node_modules/
+        },     {
             test: /\.s?css$/,
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
             ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+            }]
+        }, 
+        plugins: [
+            CSSExtract
+            ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
+
+
 
 
