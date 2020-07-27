a956c5714193c3a16ea6e0e5a312f7b02094d278
Sam Reichle <samjhreichle@gmail.com>
2020-07-27 13:41:04 -0500

    Added better formatting for pdf generator

--- src/components/FormGeneratorPage.js
+++ src/components/FormGeneratorPage.js
@@ -12,25 +12,55 @@ class FormGeneratorPage extends React.Component{
         }
     }
 
-    generatePDF = () => {
-        var doc = new jsPDF();
-      
-        doc.text('Professional Development Planner', 35, 15)
+addWrappedText = ({text, textWidth, doc, fontSize = 10, fontType = 'normal', lineSpacing = 7, xPosition = 10, initialYPosition = 10, pageWrapInitialYPosition = 10}) => {
+    var textLines = doc.splitTextToSize(text, textWidth); // Split the text into lines
+    var pageHeight = doc.internal.pageSize.height;        // Get page height, well use this for auto-paging
+    doc.setFontType(fontType);
+    doc.setFontSize(fontSize);
 
-        doc.setFont('helvetica')
-        doc.setFontType('normal')
-        const docText = this.state.goals.map((goal) => {
-            const newDeadline=  (moment(goal.deadline));
-                    return (`Goal: ${goal.goalName} ${'\n'} Strategies: ${goal.strategies} Deadline: ${newDeadline.format('MMMM Do, YYYY').toString()}`)
-                })
+    var cursorY = initialYPosition;
 
-        doc.text(docText, 0, 25)
+    textLines.forEach(lineText => {
+        if (cursorY > pageHeight) { // Auto-paging
+        doc.addPage();
+        cursorY = pageWrapInitialYPosition;
+        }
+        doc.text(xPosition, cursorY, lineText);
+        cursorY += lineSpacing;
+    })
+}
+generatePDF = () => {
 
-      doc.save('demo.pdf')
-    }   
+    let goals = this.state.goals;
+
+        const doc = new jsPDF();
+            for (var i = 0; i < goals.length; i++) {
+                let count = i + 1;
+                let goals = this.state.goals;
+                let newDeadline=  (moment(goals[i].deadline));
+
+                this.addWrappedText({
+                    text: (count.toString() + '.' + '\n'
+                                            + 'GOAL:' + '\n' + '        ' + goals[i].goalName + '\n'
+                                            + 'STRATEGIES: ' + '\n' + '         ' + goals[i].strategies + '\n'
+                                            + 'DEADLINE: ' + '\n' + '       ' + newDeadline.format('MMMM Do, YYYY').toString()), // Put a really long string here
+                    textWidth: 220,
+                    doc,
+                  
+                    // Optional
+                    fontSize: '12',
+                    fontType: 'normal',
+                    lineSpacing: 7,               // Space between lines
+                    xPosition: 10,                // Text offset from left of document
+                    initialYPosition: 30 + i * 50,         // Initial offset from top of document; set based on prior objects in document
+                    pageWrapInitialYPosition: 10  // Initial offset from top of document when page-wrapping
+                  });
+            }
+
+        doc.save('demo.pdf');
+}
     
    render() {
-       console.log(this.state.goals);
       return (
          <div>
             <button onClick={this.generatePDF} type="primary">Download PDF</button> 
